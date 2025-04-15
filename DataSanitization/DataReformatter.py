03e97a4389c8b0d62f91d7700ee9d956ab0265ed
Joshua Martin <joshuamartintx@gmail.com>
2025-04-14 20:07:44 -0500

    Added basic scripts to parse out key data from bulk HIOS data

---
+++ DataSanitization/DataReformatter.py
@@ -0,0 +1,33 @@
+import sys
+import csv, json
+
+# exit script if the file directory isn't provided
+print(len(sys.argv))
+print(sys.argv[1])
+if len(sys.argv) < 1:
+    print('exiting')
+    exit
+
+# set known delimiter
+delimiter = '|'
+
+# open provided file
+with open(sys.argv[1], "r", encoding="utf-8") as datfile:
+    # strip fields for header
+    header = datfile.readline().strip().split(delimiter)
+    print(header)
+
+    # open the output file
+    with open('output.csv', 'w', newline='',  encoding='utf-8') as csvfile:
+        spamreader = csv.DictReader(datfile, header, delimiter='|')
+        writer = csv.DictWriter(csvfile, header)
+        writer.writeheader()
+        
+        # read data
+        i = 0 # counter for total number of plans
+        numTX = 0 # counter for number of texas plans
+        for row in spamreader:
+            if row['Service Area ID'][0:2] == 'TX': # service IDs are formatted by 'abbreviated state', 'S', '###'
+                writer.writerow(row)
+
+exit
---
+++ DataSanitization/ProviderInfoReformatter.py
@@ -0,0 +1,37 @@
+import sys
+import csv, json
+
+# exit script if the file directory isn't provided
+print(len(sys.argv))
+print(sys.argv[1])
+if len(sys.argv) < 1:
+    print('exiting')
+    exit
+
+# set known delimiter
+delimiter = '|'
+
+# open provided file
+with open(sys.argv[1], "r", encoding="utf-8") as datfile:
+    # strip fields for header
+    header = datfile.readline().strip().split(delimiter)
+    print(header)
+
+    # open the output file
+    with open('output.csv', 'w', newline='',  encoding='utf-8') as csvfile:
+        spamreader = csv.DictReader(datfile, header, delimiter='|')
+        writer = csv.DictWriter(csvfile, header)
+        writer.writeheader()
+        
+        # read data
+        countyNames = []
+        i = 0 # counter for total number of plans
+        numTX = 0 # counter for number of texas plans
+        for row in spamreader:
+            if row['Service Area ID'][0:2] == 'TX': # service IDs are formatted by 'abbreviated state', 'S', '###'
+                writer.writerow(row)
+                if row['County Name'] not in countyNames and row['County Name'] != '':
+                    countyNames.append(row['County Name'])
+        
+        print('number of counties: ', len(countyNames))
+        print('counties: ', countyNames)
